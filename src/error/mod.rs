b7679c100df902821febaa733dde481cfafcbab3
akayj <askingyj@gmail.com>
2022-04-02 02:14:50 +0800

    ignore .DS_Store and others

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -14,17 +14,28 @@ impl fmt::Display for EmptyVec {
 
 impl error::Error for EmptyVec {}
 
-#[allow(dead_code)]
+// fn double_first(vec: Vec<&str>) -> Result<i32> {
+//     vec.first()
+//         // Change the error to our new type.
+//         .ok_or_else(|| EmptyVec.into())
+//         .and_then(|s| {
+//             s.parse::<i32>()
+//                 // Update to the new error type here also.
+//                 .map_err(|e| e.into())
+//                 .map(|i| 2 * i)
+//         })
+// }
+
 fn double_first(vec: Vec<&str>) -> Result<i32> {
-    vec.first()
-        // Change the error to our new type.
-        .ok_or_else(|| EmptyVec.into())
-        .and_then(|s| {
-            s.parse::<i32>()
-                // Update to the new error type here also.
-                .map_err(|e| e.into())
-                .map(|i| 2 * i)
-        })
+    let first = vec.first().ok_or(EmptyVec)?;
+    let parsed = first.parse::<i32>()?;
+    Ok(2 * parsed)
+    // .and_then(|s| {
+    //     s.parse::<i32>()
+    //         // Update to the new error type here also.
+    //         .map_err(|e| e.into())
+    //         .map(|i| 2 * i)
+    // })
 }
 
 #[allow(dead_code)]
