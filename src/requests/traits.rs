8c2a0a9269ca1480f0667ce39c9112e754208464
akayj <askingyj@gmail.com>
2023-02-12 22:02:21 +0800

    update

--- src/requests/traits.rs
+++ src/requests/traits.rs
@@ -20,16 +20,16 @@ impl<T, U> DoubleDrop<T> for U {
     fn double_drop(self, _: T) {}
 }
 
-fn make_adder_function(y: i32) -> impl Fn(i32) -> i32 {
-    move |x: i32| x + y
-}
+// fn make_adder_function(y: i32) -> impl Fn(i32) -> i32 {
+//     move |x: i32| x + y
+// }
 
-pub fn make_adder() {
-    let y = 12;
-    let plus_one = make_adder_function(y);
-    // assert_eq!(plus_one(2), 3);
-    assert_eq!(plus_one(2), 14);
-}
+// pub fn make_adder() {
+//     let y = 12;
+//     let plus_one = make_adder_function(y);
+//     // assert_eq!(plus_one(2), 3);
+//     assert_eq!(plus_one(2), 14);
+// }
 
 // 还可以使用 `impl Trait` 返回使用 `map` 或 `filter` 闭包的迭代器
 fn double_positives<'a>(numbers: &'a [i32]) -> impl Iterator<Item = i32> + 'a {
